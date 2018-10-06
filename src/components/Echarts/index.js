45c9b9ca288919d2dced2da9b22f4c67fae95724
tuonina <976056042@qq.com>
2018-10-06 21:11:39 +0800

    '修复Android发布不显示问题'

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,42 +1,54 @@
-import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, View, WebView,ViewPropTypes} from 'react-native';
 import renderChart from './renderChart';
-import echarts from './echarts.min';
+import PropTypes from 'prop-types'
+
+const isIos = Platform.OS === 'ios';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.setNewOption = this.setNewOption.bind(this);
-  }
-  
+    static propTypes={
+        ...ViewPropTypes,
+        scrollEnabled:PropTypes.bool
+    };
 
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
-      this.refs.chart.reload();
+    static defaultProps={
+        ...View.defaultProps,
+        scrollEnabled:true
+    };
+
+    constructor(props) {
+        super(props);
+        this.setNewOption = this.setNewOption.bind(this);
     }
-  }
 
-  setNewOption(option) {
-    this.refs.chart.postMessage(JSON.stringify(option));
-  }
 
-  render() {
-    return (
-      <View style={{flex: 1, height: this.props.height || 400,}}>
-        <WebView
-          ref="chart"
-          scrollEnabled = {false}
-          injectedJavaScript = {renderChart(this.props)}
-          style={{
-            height: this.props.height || 400,
-            backgroundColor: this.props.backgroundColor || 'transparent'
-          }}
-          scalesPageToFit={Platform.OS !== 'ios'}
-          source={require('./tpl.html')}
-          onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
-        />
-      </View>
-    );
-  }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.option !== this.props.option) {
+            this.refs.chart.reload();
+        }
+    }
+
+    setNewOption(option) {
+        this.refs.chart.postMessage(JSON.stringify(option));
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1, height: this.props.height || 400,}}>
+                <WebView
+                    ref="chart"
+                    scrollEnabled={this.props.scrollEnabled}
+                    injectedJavaScript={renderChart(this.props)}
+                    style={{
+                        height: this.props.height || 400,
+                        backgroundColor: this.props.backgroundColor || 'transparent'
+                    }}
+                    scalesPageToFit={Platform.OS !== 'ios'}
+                    source={isIos ? require('./tpl.html') : {uri: 'file:///android_asset/tpl.html'}}
+                    onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
+                />
+            </View>
+        );
+    }
 }
